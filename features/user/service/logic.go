cef441aca38471eb746dd1d67db67a450ef6e3eb
mfaishal882 <mfaishal882@gmail.com>
2022-11-30 06:59:41 +0700

    add helper error

--- features/user/service/logic.go
+++ features/user/service/logic.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"14-api-clean-arch/features/user"
+	"14-api-clean-arch/utils/helper"
 	"errors"
 
 	"github.com/go-playground/validator/v10"
@@ -53,19 +54,39 @@ func (service *userService) Create(input user.Core) (err error) {
 // GetAll implements user.ServiceInterface
 func (service *userService) GetAll() (data []user.Core, err error) {
 	data, err = service.userRepository.GetAll()
-	return
-
+	return data, err
 }
 
 func (service *userService) GetById(id int) (data user.Core, err error) {
+	data, err = service.userRepository.GetById(id)
+	if err != nil {
+		log.Error(err.Error())
+		return user.Core{}, helper.ServiceErrorMsg(err)
+	}
+	return data, err
 
-	return user.Core{}, err
 }
 
 func (service *userService) Update(input user.Core, id int) error {
+	if input.Password != "" {
+		generate, _ := bcrypt.GenerateFromPassword([]byte(input.Password), 10)
+		input.Password = string(generate)
+	}
+
+	_, err := service.userRepository.Update(input, id)
+	if err != nil {
+		log.Error(err.Error())
+		return helper.ServiceErrorMsg(err)
+	}
+
 	return nil
 }
 
 func (service *userService) Delete(id int) error {
+	_, err := service.userRepository.Delete(id)
+	if err != nil {
+		log.Error(err.Error())
+		return helper.ServiceErrorMsg(err)
+	}
 	return nil
 }
